13fdd605baa483a56048d7a9039676417a0ece0d
jobvancreij <jobvancrey@hotmail.com>
2022-10-09 17:26:27 +0200

    fixes chapter 1 #9

--- fps/src/main/scala/chapter01/simpleExamp.scala
+++ fps/src/main/scala/chapter01/simpleExamp.scala
@@ -12,58 +12,54 @@ package course.chapter01
 //  def buyCoffee(cc: CreditCard, p: Payments)
 //}
 
-class Coffee {
+class Coffee:
   val price = 2.50
-}
 
- class CreditCard {
-  var valOnCard: Double = 100.00 //have to change it but cannot access vars
-  def charge(amount: Double): Unit = {
-    valOnCard = valOnCard - amount // fix notation
-  }
+case class CreditCard(valOnCard: Double = 100):
+  def charge(amount: Double): CreditCard =
+    copy(valOnCard = valOnCard - amount)
 
-}
 
-case class ChargeCard(card: CreditCard, amount: Double) { // why case class?
-  def combine(other: ChargeCard): ChargeCard =  {
+
+case class ChargeCard(card: CreditCard, amount: Double):
+  def combine(other: ChargeCard): ChargeCard =
     if (card == other.card)
-      ChargeCard(card, amount + other.amount)
+      copy(amount = amount + other.amount)
     else
       throw new Exception("different cards")
-  }
 
-}
-class Cafe {
-  def buyCoffee(card: CreditCard): (Coffee,  ChargeCard) = {
+
+
+class Cafe:
+  def buyCoffee(card: CreditCard): (Coffee,  ChargeCard) =
     val cup = new Coffee()
     (cup, ChargeCard(card: CreditCard, cup.price))
-  }
 
-    def buyCoffees(card: CreditCard, n: Int): (List[Coffee], ChargeCard) = {
-      val purchases: List[(Coffee, ChargeCard)] = List.fill(n)(buyCoffee(card))
-      //val (Coffees: List[Coffee], Charges: List[ChargeCard]) = purchases.unzip
-      val (z,t) = purchases.unzip
-      (z,t.reduce((x, y) => x.combine(y)))
+  def buyCoffees(card: CreditCard, n: Int): (List[Coffee], ChargeCard) =
+    val purchases: List[(Coffee, ChargeCard)] = List.fill(n)(buyCoffee(card))
+    //val (Coffees: List[Coffee], Charges: List[ChargeCard]) = purchases.unzip
+    val (z,t) = purchases.unzip
+    (z,t.reduce((x, y) => x.combine(y)))
 
 
- }
-}
+
 
 
 
 object simpleExamp {
 
-  def main(args: Array[String]): Unit = {
-    val currentCard = new CreditCard()
+  def main(args: Array[String]): Unit =
+    val currentCard = CreditCard()
     val cafe = new Cafe()
-    val sales = cafe.buyCoffees(currentCard,4)
+    val sales = cafe.buyCoffees(currentCard, 4)
     println(sales)
-    currentCard.charge(sales._2.amount)
-    println(currentCard.valOnCard)
+    val chargedCc: CreditCard = currentCard.charge(sales._2.amount)
+    println(chargedCc.valOnCard)
 
 
-  }
 }
+
+
 
 
 
