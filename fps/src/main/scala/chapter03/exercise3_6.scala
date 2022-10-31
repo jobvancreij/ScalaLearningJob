b5eaa0cee573d03b78354cf6034b51283683d1e7
jobvancreij <jobvancrey@hotmail.com>
2022-10-31 21:23:33 +0100

    exercise 3.6

--- fps/src/main/scala/chapter03/exercise3_6.scala
+++ fps/src/main/scala/chapter03/exercise3_6.scala
@@ -1,7 +1,13 @@
-//package chapter03
-//
-//object exercise3_6 extends App:
-//  def init[A](l: List[A]): List[A] =
-//    l.reverse.tail.reverse
-//
-//  println(init(List(1,2,3,4)))
+package chapter03
+import chapter03.listImplementation.{Cons, Nil, List}
+
+object exercise3_6 extends App:
+  def dropLast[A](l: List[A]): List[A] =
+    l match {
+      case Cons(_,Nil) => Nil
+      case Cons(x,y) => Cons(x, dropLast(y))
+    }
+
+  println(dropLast(List(1,2,3,4)))
+  // Linked list work from front to back
+  // So have to iterate through entire list
