84cc87414cab1ad845cc1cc9debd25e38700d3ca
jobvancreij <jobvancrey@hotmail.com>
2022-11-11 11:21:29 +0100

    make 3.16 shorter

--- fps/src/main/scala/chapter03/exercise3_16.scala
+++ fps/src/main/scala/chapter03/exercise3_16.scala
@@ -1,6 +1,7 @@
 
 package chapter03
 
+import chapter03.exercise3_17.doubleToString
 import chapter03.listImplementation.*
 import chapter03.listImplementation.List.*
 
@@ -10,11 +11,10 @@ import scala.annotation.tailrec
 object exercise3_16 extends App:
 
   def addOne(as: List[Int]): List[Int] =
-    println(as)
     as match {
       case Nil => Nil
-      case Cons(x, Nil) => Cons(x + 1, Nil)
-      case Cons(x, y) => Cons(x + 1, addOne(y))
+      case Cons(x, y) => Cons(x+1, if (y == Nil) Nil else addOne(y))
+
     }
 
   println(addOne(List(1,2,3,4)))
