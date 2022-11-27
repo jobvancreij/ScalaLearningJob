f002578da70bd4f1435281cfbdd9be95b09ba3e2
jobvancreij <jobvancrey@hotmail.com>
2022-11-27 23:52:19 +0100

    day 4 finish

--- aoc/src/main/scala/aoc2015/exercise04.scala
+++ aoc/src/main/scala/aoc2015/exercise04.scala
@@ -4,7 +4,6 @@ import java.security.MessageDigest
 
 object exercise04 extends App:
 
-
   def diyMD5(str: String): String =
     MessageDigest
       .getInstance("MD5")
@@ -12,15 +11,13 @@ object exercise04 extends App:
       .map("%02x".format(_))
       .mkString("")
 
-  def looper(std: String, nbrZeros: Int,  extr: Int = 0): String =
+
+  // Graag samen naar kijken
+  def looper(std: String, nbrZeros: Int,  extr: Int = 0): Int =
   // can I make the inner func that I only have to init with the nbr of zeros once?
-    def inner(nbrZero: String)(std: String, extr: Int):  String  =
-      val newInput = s"$std${extr.toString}"
-      val newTest = diyMD5(newInput)
-      newTest.take(5) match {
-        case `nbrZero`  => newInput
-        case _ => inner(nbrZero)(std, extr + 1)
-      }
+    def inner(nbrZero: String)(std: String, extr: Int):  Int  =
+      val newTest = diyMD5(s"$std${extr.toString}")
+      if newTest.take(nbrZeros)  == nbrZero then extr else inner(nbrZero)(std, extr + 1)
     inner("0" * nbrZeros)(std, 0)
 
 
