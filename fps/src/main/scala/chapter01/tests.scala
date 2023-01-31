b20f59c97a10ec3d0ab0087a37efb9e212e472a8
Job van Creij <33804555+jobvancreij@users.noreply.github.com>
2023-01-31 16:38:40 +0100

    new start book and aoc2015 (#19)

--- fps/src/main/scala/chapter01/tests.scala
+++
@@ -1,5 +0,0 @@
-package chapter01
-
-case object tests extends App {
-  println(List(("a","b"), ("c", "d")).unzip)
-}
--- fps/src/main/scala/chapter03/exercise3_7.scala
+++
@@ -1,8 +0,0 @@
-package chapter03
-
-import chapter03.listImplementation.{Cons, List, Nil}
-
-object exercise3_7 extends App:
-  println("hallo world")
-  // No its a recursive function and cannot change previous outputs
-  // So if third item is 0 cannot change first two
